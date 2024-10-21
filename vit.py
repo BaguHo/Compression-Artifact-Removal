a1b943aedadd2c3f4fc7c3bc2d9e60f304edfff2
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2024-10-21 16:21:29 +0900

    Fix the num_classes 10 to 5 in vit.py

--- vit.py
+++ vit.py
@@ -14,27 +14,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.metrics import precision_score, confusion_matrix
 import os
-import shutil
-import cv2
-import random
 import numpy as np
 import time
 from sklearn.model_selection import train_test_split
-from timm import create_model
 
-# TODO: GPU에 따라 다르게 설정
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-# device = torch.device('mps')
-print(device)
 
 channels = 1
 learning_rate = 0.001
 epochs = 1
 batch_size = 64
-QF = 60
 dataset_name = "Tufts Face Database"
 model_name = "ViT"
-num_workers = 4
+num_workers = 2
 image_type = 'RGB'
 
 # 디렉토리 생성
@@ -44,7 +35,7 @@ def makedir(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-# 모델 저장
+# save model
 
 
 def save_model(model, path, filename):
@@ -54,7 +45,7 @@ def save_model(model, path, filename):
     torch.save(model.state_dict(), model_path)
     print(f"Model saved to {model_path}")
 
-# 모델 학습 함수
+# model training
 
 
 def train(model, train_loader, criterion, optimizer):
@@ -63,8 +54,7 @@ def train(model, train_loader, criterion, optimizer):
     for epoch in range(epochs):
         running_loss = 0.0
         for images, labels in tqdm(train_loader, desc=f"Epoch {epoch+1}/{epochs}", leave=False):
-            # if len(images.shape) == 3:
-            #     images = images.unsqueeze(0)
+            images, labels = images.to(device), labels.to(device)
 
             optimizer.zero_grad()
 
@@ -79,7 +69,7 @@ def train(model, train_loader, criterion, optimizer):
         print(f'Epoch [{epoch+1}/{epochs}], Loss: {running_loss/len(train_loader):.4f}')
 
 
-# 모델 평가
+# evaluate model
 def test(model, test_loader, msg):
     model.eval()
     correct = 0
@@ -107,10 +97,10 @@ def test(model, test_loader, msg):
 
     return accuracy, precision_avg
 
-# 결과 저장
+# save result
 
 
-def save_result(model_name=model_name,  train_dataset=None, test_dataset=None, accuracy=None, precision=None, QF=QF):
+def save_result(model_name=model_name,  train_dataset=None, test_dataset=None, accuracy=None, precision=None, QF=None):
     results_df = pd.DataFrame({
         'Model Name': [model_name],
         "Channel": [channels],
@@ -138,36 +128,44 @@ def make_jpeg_datasets(QF):
     makedir(train_output_dir)
     makedir(test_output_dir)
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-        # transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    ])
-
-    # transform = transforms.Compose([
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    # ])
-
     # original dataset load
     original_dataset_path = './datasets/thermal_cropped_images'
     original_dataset = datasets.ImageFolder(original_dataset_path, transform=transform)
     original_dataset_train, original_dataset_test = train_test_split(original_dataset, test_size=0.2, random_state=42)
 
     # make JPEG dataset
-    for i in range(len(5)):
-        makedir(os.path.join(train_output_dir, "class_" + str(i)))
-        makedir(os.path.join(test_output_dir, "class_" + str(i)))
+    for i in range(5):
+        makedir(os.path.join(train_output_dir, str(i + 1)))
+        makedir(os.path.join(test_output_dir, str(i + 1)))
 
-    for idx, (image, label) in enumerate(original_dataset_train):
-        file_name = f"image_{idx}_label_{label}.jpg"
-        output_file_path = os.path.join(train_output_dir, "class_" + str(label), file_name)
-        image.convert(image_type).save(output_file_path, 'JPEG', quality=QF)
+    # Define a transform to convert tensors to PIL images
+    to_pil = transforms.ToPILImage()
 
-    for idx, (image, label) in enumerate(original_dataset_test):
-        file_name = f"image_{idx}_label_{label}.jpg"
-        output_file_path = os.path.join(test_output_dir, "class_" + str(label), file_name)
-        image.convert(image_type).save(output_file_path, 'JPEG', quality=QF)
+    # Save train images as JPEG
+    for idx, (image_tensor, label) in enumerate(original_dataset_train):
+        image = to_pil(image_tensor)
+        file_name = f"{idx}_label_{label + 1}.jpg"
+        output_file_path = os.path.join(train_output_dir, str(label + 1), file_name)
+        image.save(output_file_path, 'JPEG', quality=QF)
+        # print(f'saved {output_file_path}')
+
+    # Save test images as JPEG
+    for idx, (image_tensor, label) in enumerate(original_dataset_test):
+        image = to_pil(image_tensor)
+        file_name = f"image_{idx}_label_{label + 1}.jpg"
+        output_file_path = os.path.join(test_output_dir, str(label + 1), file_name)
+        image.save(output_file_path, 'JPEG', quality=QF)
+        # print(f'saved {output_file_path}')
+
+    # for idx, (image, label) in enumerate(original_dataset_train):
+    #     file_name = f"image_{idx}_label_{label}.jpg"
+    #     output_file_path = os.path.join(train_output_dir, str(label), file_name)
+    #     image.convert(image_type).save(output_file_path, 'JPEG', quality=QF)
+
+    # for idx, (image, label) in enumerate(original_dataset_test):
+    #     file_name = f"image_{idx}_label_{label}.jpg"
+    #     output_file_path = os.path.join(test_output_dir, "class_" + str(label), file_name)
+    #     image.convert(image_type).save(output_file_path, 'JPEG', quality=QF)
 
 # JPEG 데이터셋 로드
 # TODO: dataset에  따라 변경 필요함
@@ -179,6 +177,9 @@ def load_jpeg_datasets(QF, transform):
 
     train_dataset = datasets.ImageFolder(jpeg_train_dir, transform=transform)
     test_dataset = datasets.ImageFolder(jpeg_test_dir, transform=transform)
+
+    # print(f'train_dataset shape: {train_dataset}')
+    # print(f'test_dataset shape: {test_dataset}')
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
                                   num_workers=num_workers, drop_last=True)
@@ -195,22 +196,28 @@ def tensor_to_image(tensor):
     return image_np
 
 
-def show_images(dataset, length=5):
-    random_indices = torch.randperm(len(dataset))[:length]
+def show_images(dataset, dataloader, length=5):
+    random_indices = torch.randperm(len(dataloader))[:length]
 
-    plt.figure(figsize=(10, 2))
+    # 데이터 로더에서 배치 가져오기
+    data_iter = iter(dataloader)
+    images, labels = next(data_iter)
 
-    for i, idx in enumerate(random_indices):
-        image_tensor, label = dataset[idx]
-        image_np = tensor_to_image(image_tensor)
+    # 클래스 이름 정의 (예시)
+    class_names = dataset.classes
 
-        plt.subplot(1, length, i + 1)
-        plt.imshow(image_np, cmap='gray')
-        plt.title(f'Label: {label}')
-        plt.axis('off')
+    # 이미지 시각화 함수 정의
+    def imshow(img, label):
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.title(class_names[label])
+        plt.show()
 
-    plt.tight_layout()
-    plt.show()
+    # 몇 개의 이미지 시각화
+    num_images_to_show = 5
+    for i in range(num_images_to_show):
+        imshow(images[i], labels[i].item())
+
 
 # ViT 모델 정의
 
@@ -238,7 +245,7 @@ class Encoder(nn.Module):
 
 
 class ViT(nn.Module):
-    def __init__(self, in_channels=3, num_encoders=6, embed_size=768, img_size=(128, 128), patch_size=16, num_classes=10, num_heads=4):
+    def __init__(self, in_channels=3, num_encoders=6, embed_size=768, img_size=(128, 128), patch_size=16, num_classes=5, num_heads=4):
         super().__init__()
         self.img_size = img_size
         self.patch_size = patch_size
@@ -271,18 +278,7 @@ class ViT(nn.Module):
 
 # training & testing for each QF
 def training_testing():
-    QFs = [20, 40, 60, 80, 100]
-
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-        # transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    ])
-
-    # transform = transforms.Compose([
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    # ])
+    QFs = [100, 80, 60, 40, 20]
 
     # original dataset load
     original_dataset_path = './datasets/thermal_cropped_images'
@@ -294,39 +290,44 @@ def training_testing():
     original_dataset_test_loader = DataLoader(
         original_dataset_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-    train_sample, _ = next(iter(original_dataset_train_loader))
+    train_sample, train_labels = next(iter(original_dataset_train_loader))
     print(f'train_shape: {train_sample.shape}')
+    print(f'train labels:  {train_labels}')
 
-    test_sample, _ = next(iter(original_dataset_test_loader))
+    test_sample, test_labels = next(iter(original_dataset_test_loader))
     print(f'test_shape: {test_sample.shape}')
+    print(f'test labels:  {test_labels}')
 
     # original model
     original_model = ViT().to(device)
-    # original_model = create_model('vit_base_patch16_224', pretrained=False, num_classes=5, img_size=[128, 128])
-    # original_model.patch_embed.proj = nn.Conv2d(3, original_model.embed_dim, kernel_size=(16, 16), stride=(8, 8))
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(original_model.parameters(), lr=learning_rate)
 
     # original dataset model tarining
+    print('[train the original model]')
     train(original_model, original_dataset_train_loader, criterion, optimizer)
 
     # save original model
+    print('save original model')
     save_model(original_model, './models', 'original_model.pth')
 
     for QF in QFs:
         # JPEG dataset 생성
+        print('make jpeg dataset')
+        print('#############################################################################')
         make_jpeg_datasets(QF)
 
         # load JPEG  datasets
-        _, _,  jpeg_train, jpeg_test = load_jpeg_datasets(QF, transform)
+        jpeg_train_dataset, jpeg_test_dataset,  jpeg_train_loader, jpeg_test_loader = load_jpeg_datasets(QF, transform)
 
         # test with original dataset test dataset
         accuracy, precision = test(original_model, original_dataset_test_loader, 'original - original')
         save_result(model_name, dataset_name,  dataset_name, accuracy, precision, QF)
 
         #  test with JPEG test dataset
-        accuracy, precision = test(original_model, jpeg_test, f'original - jpeg {QF}')
+        print('test original model with JPEG test dataset')
+        accuracy, precision = test(original_model, jpeg_test_loader, f'original - jpeg {QF}')
         save_result(model_name, dataset_name, f'JPEG', accuracy, precision)
 
         # Tarining with JPEG dataset.
@@ -335,26 +336,66 @@ def training_testing():
 
         # jpeg_model.patch_embed.proj = nn.Conv2d(3, jpeg_model.embed_dim, kernel_size=(16, 16), stride=(8, 8))
 
-        # 손실함수 정의
+        # JPEG model 손실함수 정의
+        criterion = nn.CrossEntropyLoss()
         optimizer = optim.Adam(jpeg_model.parameters(), lr=learning_rate)
 
-        # train the jpeg modelYou gonna learn Looking at that. How to maintain a look at the deal Yeah. who also pulled up. into the Now you guys
-        train(jpeg_model, jpeg_train, criterion, optimizer)
+        # train the jpeg model
+        print('[train the jpeg model]')
+        train(jpeg_model, jpeg_train_loader, criterion, optimizer)
 
         # save jpeg model
         save_model(jpeg_model, './models', 'jpeg_model.pth')
 
         # Test with JPEG test dataset
-        accuracy, precision = test(jpeg_model, jpeg_test, f'jpeg {QF} - jpeg {QF}')
+        print('test jpeg model with JPEG test dataset')
+        accuracy, precision = test(jpeg_model, jpeg_test_loader, f'jpeg {QF} - jpeg {QF}')
         save_result(model_name, f'JPEG', f'JPEG', accuracy, precision)
 
         # test with original  test dataset
-        accuracy, precision = test(jpeg_model, original_dataset_test, f'jpeg {QF} - original')
+        print('test jpeg model with original  test dataset')
+        accuracy, precision = test(jpeg_model, original_dataset_test_loader, f'jpeg {QF} - original')
         save_result(model_name, f'JPEG', dataset_name, accuracy, precision)
+        print('#############################################################################')
 
 
 ################################################################################################################
 ################################################################################################################
 
 if __name__ == "__main__":
+
+    # transform 정의
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        # transforms.RandomRotation([-30, 30]),
+        # transforms.RandomHorizontalFlip(),
+    ])
+
+    # make_jpeg_datasets(100)
+
+    # gpu 설정
+    # device = 'cuda' if torch.cuda.is_available() else ('mps' if torch.backends.mps.is_available() else 'cpu')
+    # print(device)
+    device = 'cpu'
+    print(device)
+
     training_testing()
+
+    # # original dataset load
+    # original_dataset_path = './datasets/thermal_cropped_images'
+    # original_dataset = datasets.ImageFolder(original_dataset_path, transform=transform)
+    # original_dataset_train, original_dataset_test = train_test_split(original_dataset, test_size=0.2, random_state=42)
+
+    # original_dataset_train_loader = DataLoader(
+    #     original_dataset_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    # original_dataset_test_loader = DataLoader(
+    #     original_dataset_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    # # show_images(original_dataset, original_dataset_train_loader)
+
+    # # show_images(original_dataset, original_dataset_test_loader)
+
+    # jpeg_train, jpeg_test, jpeg_train_loader, jpeg_test_loader = load_jpeg_datasets(100, transform)
+
+    # show_images(jpeg_train, jpeg_train_loader)
+    # show_images(jpeg_test, jpeg_test_loader)
