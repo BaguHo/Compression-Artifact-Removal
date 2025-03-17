7b5033b83a4606358586b1fee2d620a8dd3a56c8
JihoEum <say7fish@gmail.com>
2025-03-17 10:12:37 +0900

    Update train_multiple_classification_models.py

--- code/train_multiple_classification_models.py
+++ code/train_multiple_classification_models.py
@@ -15,8 +15,6 @@ import timm
 import re
 from knockknock import slack_sender
 
-slack_webhook_url = ("https://hooks.slack.com/services/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")
-
 learning_rate = 0.001
 epochs = 40
 batch_size = 512
