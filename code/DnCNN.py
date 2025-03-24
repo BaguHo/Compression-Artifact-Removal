f1c6682d22a2299fb7557a1eb0538c5278e684d7
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2025-03-24 18:48:18 +0900

    Add PxT v2

---
+++ code/DnCNN.py
@@ -0,0 +1,337 @@
+from skimage.metrics import structural_similarity, peak_signal_noise_ratio
+from torchmetrics.image import PeakSignalNoiseRatioWithBlockedEffect
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
+import numpy as np
+from torch import nn
+import torch
+import os, sys, re
+import logging
+import cv2
+import tqdm
+
+if len(sys.argv) < 5:
+    print("Usage: python script.py <epoch> <batch_size> <num_workers> <num_classes>")
+    sys.exit(1)
+
+logging.basicConfig(
+    filename="data.log", level=logging.INFO, format="%(asctime)s - %(message)s"
+)
+
+dataset_name = "CIFAR100"
+slack_webhook_url = (
+    "https://hooks.slack.com/services/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"
+)
+epochs = int(sys.argv[1])
+batch_size = int(sys.argv[2])
+num_workers = int(sys.argv[3])
+num_classes = int(sys.argv[4])
+
+
+def sort_key(filename):
+    image_match = re.search(r"image_(\d+)", filename)
+    crop_match = re.search(r"crop_(\d+)", filename)
+
+    image_number = int(image_match.group(1)) if image_match else float("inf")
+    crop_number = int(crop_match.group(1)) if crop_match else float("inf")
+
+    return (image_number, crop_number)
+
+
+class CIFAR100Dataset(Dataset):
+    def __init__(self, input_images, target_images, transform=transforms.ToTensor()):
+        self.input_images = input_images
+        self.target_images = target_images
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.input_images)
+
+    def __getitem__(self, idx):
+        input_image = self.input_images[idx]
+        target_image = self.target_images[idx]
+
+        # input_image = cv2.imread(input_image)
+        # target_image = cv2.imread(target_image)
+        # ! warning: The following lines are commented out to avoid PIL dependency
+        # input_image = Image.fromarray(input_image)
+        # target_image = Image.fromarray(target_image)
+
+        if self.transform:
+            input_image = self.transform(input_image)
+            target_image = self.transform(target_image)
+
+        return input_image, target_image
+
+
+def load_images():
+    QFs = [80, 60, 40, 20]
+    dataset_name = "CIFAR100"
+    cifar100_path = os.path.join(os.getcwd(), "datasets", dataset_name, "original_size")
+
+    train_input_dataset = []
+    test_input_dataset = []
+    train_target_dataset = []
+    test_target_dataset = []
+
+    for QF in QFs:
+        # input images
+        train_input_dir = os.path.join(cifar100_path, f"jpeg{QF}", "train")
+        test_input_dir = os.path.join(cifar100_path, f"jpeg{QF}", "test")
+
+        # target images (original)
+        target_train_dataset_dir = os.path.join(cifar100_path, "original", "train")
+        target_test_dataset_dir = os.path.join(cifar100_path, "original", "test")
+
+        # 학습 데이터 로드
+        for i in range(num_classes):
+            train_path = os.path.join(train_input_dir, str(i))
+            target_train_path = os.path.join(target_train_dataset_dir, str(i))
+
+            # train_path 내 파일을 정렬된 순서로 불러오기
+            sorted_train_files = sorted(os.listdir(train_path), key=sort_key)
+            sorted_target_train_files = sorted(
+                os.listdir(target_train_path), key=sort_key
+            )
+
+            # 두 디렉토리의 파일명이 같은지 확인하며 로드
+            for train_file, target_file in tqdm.tqdm(
+                zip(sorted_train_files, sorted_target_train_files),
+                desc=f"Loading class {i} train data (QF {QF})",
+                total=len(sorted_train_files),
+            ):
+                if train_file.replace("jpeg", "png") == target_file:
+                    # input 이미지 로드
+                    train_image_path = os.path.join(train_path, train_file)
+                    train_image = cv2.imread(train_image_path)
+                    train_input_dataset.append(train_image)
+
+                    # target 이미지 로드
+                    target_image_path = os.path.join(target_train_path, target_file)
+                    target_image = cv2.imread(target_image_path)
+                    train_target_dataset.append(target_image)
+                else:
+                    print(
+                        f"Warning: Mismatched files in training set: {train_file} and {target_file}"
+                    )
+
+        # 테스트 데이터 로드
+        for i in tqdm.tqdm(range(num_classes), desc=f"Loading test data (QF {QF})"):
+            test_path = os.path.join(test_input_dir, str(i))
+            target_test_path = os.path.join(target_test_dataset_dir, str(i))
+
+            # test_path 내 파일을 정렬된 순서로 불러오기
+            sorted_test_files = sorted(os.listdir(test_path), key=sort_key)
+            sorted_target_test_files = sorted(
+                os.listdir(target_test_path), key=sort_key
+            )
+
+            # 두 디렉토리의 파일명이 같은지 확인하며 로드
+            for test_file, target_file in zip(
+                sorted_test_files, sorted_target_test_files
+            ):
+                if test_file.replace("jpeg", "png") == target_file:
+                    # input 이미지 로드
+                    test_image_path = os.path.join(test_path, test_file)
+                    test_image = cv2.imread(test_image_path)
+                    test_input_dataset.append(test_image)
+
+                    # target 이미지 로드
+                    target_image_path = os.path.join(target_test_path, target_file)
+                    target_image = cv2.imread(target_image_path)
+                    test_target_dataset.append(target_image)
+                else:
+                    print(
+                        f"Warning: Mismatched files in testing set: {test_file} and {target_file}"
+                    )
+
+    # Dataset과 DataLoader 생성
+    train_dataset = CIFAR100Dataset(train_input_dataset, train_target_dataset)
+    test_dataset = CIFAR100Dataset(test_input_dataset, test_target_dataset)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_dataset, test_dataset, train_loader, test_loader
+
+
+class DnCNN(nn.Module):
+    def __init__(self, num_layers=17, num_features=64):
+        super(DnCNN, self).__init__()
+        layers = [
+            nn.Sequential(
+                nn.Conv2d(3, num_features, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(inplace=True),
+            )
+        ]
+        for i in range(num_layers - 2):
+            layers.append(
+                nn.Sequential(
+                    nn.Conv2d(num_features, num_features, kernel_size=3, padding=1),
+                    nn.BatchNorm2d(num_features),
+                    nn.ReLU(inplace=True),
+                )
+            )
+        layers.append(nn.Conv2d(num_features, 3, kernel_size=3, padding=1))
+        self.layers = nn.Sequential(*layers)
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, inputs):
+        y = inputs
+        residual = self.layers(y)
+        return y - residual
+
+
+# test를 돌릴 때 psnr, ssim 를 평균으로 저장하는 함수 (.csv로 저장)
+def save_metrics(metrics, filename):
+    with open(filename, "w") as f:
+        f.write("PSNR,SSIM\n")
+        for i in range(len(metrics["PSNR"])):
+            f.write(f"{metrics['PSNR'][i]},{metrics['SSIM'][i]}\n")
+    print(f"Metrics saved to {filename}")
+
+
+if __name__ == "__main__":
+    # Load the dataset
+    train_dataset, test_dataset, train_loader, test_loader = load_images()
+
+    # Print dataset sizes
+    print(f"Train dataset size: {len(train_dataset)}")
+    print(f"Test dataset size: {len(test_dataset)}")
+
+    # Initialize the model
+    model = DnCNN()
+    print(model)
+
+    device = torch.device(
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+    # use multiple GPUs if available
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
+        print(f"Using {torch.cuda.device_count()} GPUs")
+
+    model.to(device)
+    print(f"Model device: {device}")
+
+    # train the model
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        for i, (input_images, target_images) in enumerate(
+            tqdm.tqdm(train_loader, desc=f"Train Epoch {epoch+1}/{epochs}")
+        ):
+            input_images = input_images.to(device)
+            target_images = target_images.to(device)
+
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(input_images)
+            loss = criterion(outputs, target_images)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        epoch_loss = running_loss / len(train_loader)
+        print(f"Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss:.4f}")
+        logging.info(f"Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss:.4f}")
+        # Save the model
+        if (epoch + 1) % 10 == 0:
+            torch.save(model.state_dict(), f"DnCNN_{epoch+1}.pth")
+            print(f"Model saved at epoch {epoch+1}")
+            logging.info(f"Model saved at epoch {epoch+1}")
+
+    # Test the model
+    model.eval()
+    test_loss = 0.0
+    psnr_values = []
+    ssim_values = []
+    psnr_b_values = []
+
+    with torch.no_grad():
+        for input_images, target_images in tqdm.tqdm(test_loader, desc="Testing"):
+            input_images = input_images.to(device)
+            target_images = target_images.to(device)
+
+            # Forward pass
+            outputs = model(input_images)
+
+            # Calculate MSE loss
+            loss = criterion(outputs, target_images)
+            test_loss += loss.item()
+
+            # bgr tensor to rgb image ndarray
+            target_images = target_images.permute(0, 2, 3, 1).cpu().numpy()
+            outputs = outputs.permute(0, 2, 3, 1).cpu().numpy()
+
+            for i in range(len(outputs)):
+                # [32,32,3]
+                print(f"target image shape: {target_images[i].shape}")
+                print(f"output image shape: {outputs[i].shape}")
+                # Calculate PSNR
+                psnr = peak_signal_noise_ratio(
+                    target_images[i], outputs[i], data_range=1.0
+                )
+
+                # Calculate SSIM
+                ssim = structural_similarity(
+                    target_images[i],
+                    outputs[i],
+                    multichannel=True,
+                    data_range=1.0,
+                    channel_axis=2,
+                )
+
+                psnr_values.append(psnr)
+                ssim_values.append(ssim)
+                # psnr_b_values.append(psnr_b)
+                # print(f"PSNR: {psnr:.2f}, SSIM: {ssim:.4f}, PSNR-B: {psnr_b:.2f}")
+                # logging.info(
+                #     f"PSNR: {psnr:.2f}, SSIM: {ssim:.4f}, PSNR-B: {psnr_b:.2f}"
+                # )
+                print(f"PSNR: {psnr:.2f}, SSIM: {ssim:.4f}")
+                logging.info(f"PSNR: {psnr:.2f}, SSIM: {ssim:.4f}")
+
+            # Save the output images
+            for i in range(len(outputs)):
+                output_image = cv2.cvtColor(outputs[i], cv2.COLOR_RGB2BGR)
+                output_image_path = os.path.join("output", f"output_{i}.png")
+                cv2.imwrite(output_image_path, output_image)
+                print(f"Output image saved at {output_image_path}")
+            logging.info(f"Output image saved at {output_image_path}")
+
+    # Calculate average metrics
+    avg_test_loss = test_loss / len(test_loader)
+    avg_psnr = np.mean(psnr_values)
+
+    print(f"Test Loss: {avg_test_loss:.4f}, PSNR: {avg_psnr:.2f} dB")
+    logging.info(f"Test Loss: {avg_test_loss:.4f}, PSNR: {avg_psnr:.2f} dB")
+
+    # Save metrics
+    metrics = {
+        "Test Loss": [avg_test_loss],
+        "PSNR": psnr_values,
+        "SSIM": ssim_values,
+    }
+    save_metrics(metrics, "dncnn_metrics.csv")
+    # Save the final model
+    torch.save(
+        model.state_dict(),
+        os.path.join("datasets", f"{type(model).__name__}_final.pth"),
+    )
+    print(f"Final model saved as {type(model).__name__}_final.pth")
+    logging.info(f"Final model saved as {type(model).__name__}_final.pth")
---
+++ code/PxT_v2.py
@@ -0,0 +1,395 @@
+from skimage.metrics import structural_similarity, peak_signal_noise_ratio
+from torchmetrics.image import PeakSignalNoiseRatioWithBlockedEffect
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
+import numpy as np
+from torch import nn
+import torch
+import os, sys, re
+import logging
+import cv2
+import tqdm
+
+if len(sys.argv) < 5:
+    print("Usage: python script.py <epoch> <batch_size> <num_workers> <num_classes>")
+    sys.exit(1)
+
+logging.basicConfig(
+    filename="data.log", level=logging.INFO, format="%(asctime)s - %(message)s"
+)
+
+dataset_name = "CIFAR100"
+slack_webhook_url = (
+    "https://hooks.slack.com/services/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"
+)
+epochs = int(sys.argv[1])
+batch_size = int(sys.argv[2])
+num_workers = int(sys.argv[3])
+num_classes = int(sys.argv[4])
+
+
+def sort_key(filename):
+    image_match = re.search(r"image_(\d+)", filename)
+    crop_match = re.search(r"crop_(\d+)", filename)
+
+    image_number = int(image_match.group(1)) if image_match else float("inf")
+    crop_number = int(crop_match.group(1)) if crop_match else float("inf")
+
+    return (image_number, crop_number)
+
+
+class CIFAR100Dataset(Dataset):
+    def __init__(self, input_images, target_images, transform=transforms.ToTensor()):
+        self.input_images = input_images
+        self.target_images = target_images
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.input_images)
+
+    def __getitem__(self, idx):
+        input_image = self.input_images[idx]
+        target_image = self.target_images[idx]
+
+        # input_image = cv2.imread(input_image)
+        # target_image = cv2.imread(target_image)
+        # ! warning: The following lines are commented out to avoid PIL dependency
+        # input_image = Image.fromarray(input_image)
+        # target_image = Image.fromarray(target_image)
+
+        if self.transform:
+            input_image = self.transform(input_image)
+            target_image = self.transform(target_image)
+
+        return input_image, target_image
+
+
+def load_images():
+    QFs = [80, 60, 40, 20]
+    dataset_name = "CIFAR100"
+    cifar100_path = os.path.join(os.getcwd(), "datasets", dataset_name, "8x8_images")
+
+    train_input_dataset = []
+    test_input_dataset = []
+    train_target_dataset = []
+    test_target_dataset = []
+
+    for QF in QFs:
+        # input images
+        train_input_dir = os.path.join(cifar100_path, f"jpeg{QF}", "train")
+        test_input_dir = os.path.join(cifar100_path, f"jpeg{QF}", "test")
+
+        # target images (original)
+        target_train_dataset_dir = os.path.join(cifar100_path, "original", "train")
+        target_test_dataset_dir = os.path.join(cifar100_path, "original", "test")
+
+        # 학습 데이터 로드
+        for i in range(num_classes):
+            train_path = os.path.join(train_input_dir, str(i))
+            target_train_path = os.path.join(target_train_dataset_dir, str(i))
+
+            # train_path 내 파일을 정렬된 순서로 불러오기
+            sorted_train_files = sorted(os.listdir(train_path), key=sort_key)
+            sorted_target_train_files = sorted(
+                os.listdir(target_train_path), key=sort_key
+            )
+
+            # 두 디렉토리의 파일명이 같은지 확인하며 로드
+            for train_file, target_file in tqdm.tqdm(
+                zip(sorted_train_files, sorted_target_train_files),
+                desc=f"Loading class {i} train data (QF {QF})",
+                total=len(sorted_train_files),
+            ):
+                if train_file.replace("jpeg", "png") == target_file:
+                    # input 이미지 로드
+                    train_image_path = os.path.join(train_path, train_file)
+                    train_image = cv2.imread(train_image_path)
+                    train_input_dataset.append(train_image)
+
+                    # target 이미지 로드
+                    target_image_path = os.path.join(target_train_path, target_file)
+                    target_image = cv2.imread(target_image_path)
+                    train_target_dataset.append(target_image)
+                else:
+                    print(
+                        f"Warning: Mismatched files in training set: {train_file} and {target_file}"
+                    )
+
+        # 테스트 데이터 로드
+        for i in tqdm.tqdm(range(num_classes), desc=f"Loading test data (QF {QF})"):
+            test_path = os.path.join(test_input_dir, str(i))
+            target_test_path = os.path.join(target_test_dataset_dir, str(i))
+
+            # test_path 내 파일을 정렬된 순서로 불러오기
+            sorted_test_files = sorted(os.listdir(test_path), key=sort_key)
+            sorted_target_test_files = sorted(
+                os.listdir(target_test_path), key=sort_key
+            )
+
+            # 두 디렉토리의 파일명이 같은지 확인하며 로드
+            for test_file, target_file in zip(
+                sorted_test_files, sorted_target_test_files
+            ):
+                if test_file.replace("jpeg", "png") == target_file:
+                    # input 이미지 로드
+                    test_image_path = os.path.join(test_path, test_file)
+                    test_image = cv2.imread(test_image_path)
+                    test_input_dataset.append(test_image)
+
+                    # target 이미지 로드
+                    target_image_path = os.path.join(target_test_path, target_file)
+                    target_image = cv2.imread(target_image_path)
+                    test_target_dataset.append(target_image)
+                else:
+                    print(
+                        f"Warning: Mismatched files in testing set: {test_file} and {target_file}"
+                    )
+
+    # Dataset과 DataLoader 생성
+    train_dataset = CIFAR100Dataset(train_input_dataset, train_target_dataset)
+    test_dataset = CIFAR100Dataset(test_input_dataset, test_target_dataset)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_dataset, test_dataset, train_loader, test_loader
+
+
+class Encoder(nn.Module):
+    def __init__(self, embed_dim, num_heads, mlp_dim):
+        super(Encoder, self).__init__()
+        self.ln1 = nn.LayerNorm(embed_dim)
+        self.mhsa = nn.MultiheadAttention(embed_dim, num_heads)
+        self.ln2 = nn.LayerNorm(embed_dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(embed_dim, mlp_dim), nn.GELU(), nn.Linear(mlp_dim, embed_dim)
+        )
+
+    def forward(self, x):
+        # Multi-head self-attention with residual connection
+        y = self.ln1(x)
+        y, _ = self.mhsa(y, y, y)
+        x = x + y
+
+        # MLP with residual connection
+        y = self.ln2(x)
+        y = self.mlp(y)
+        x = x + y
+        return x
+
+
+class PxT(nn.Module):
+    def __init__(
+        self,
+        img_size=8,
+        patch_size=1,
+        in_channels=3,
+        embed_dim=64,
+        num_heads=16,
+        num_layers=8,
+        mlp_dim=128,
+    ):
+        super(PxT, self).__init__()
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = (img_size // patch_size) ** 2
+        self.patch_dim = in_channels * patch_size * patch_size
+        self.embed_dim = embed_dim
+
+        self.patch_embed = nn.Linear(self.patch_dim, embed_dim)
+
+        self.position_embeddings = nn.Parameter(
+            torch.zeros(1, self.num_patches, embed_dim)
+        )
+
+        self.transformer = nn.ModuleList(
+            [Encoder(embed_dim, num_heads, mlp_dim) for _ in range(num_layers)]
+        )
+
+        self.decoder = nn.Sequential(nn.Linear(embed_dim, self.patch_dim))
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        x = x.unfold(2, self.patch_size, self.patch_size).unfold(
+            3, self.patch_size, self.patch_size
+        )
+        x = x.permute(0, 2, 3, 1, 4, 5).contiguous()
+        x = x.view(batch_size, -1, self.patch_dim)
+
+        x = self.patch_embed(x)
+        x = x + self.position_embeddings
+
+        x = x.permute(1, 0, 2)
+        for layer in self.transformer:
+            x = layer(x)
+        x = x.permute(1, 0, 2)
+
+        x = self.decoder(x)
+
+        x = x.view(
+            batch_size,
+            self.img_size // self.patch_size,
+            self.img_size // self.patch_size,
+            3,
+            self.patch_size,
+            self.patch_size,
+        )
+        x = x.permute(0, 3, 1, 4, 2, 5).contiguous()
+        x = x.view(batch_size, 3, self.img_size, self.img_size)
+        return x
+
+
+# test를 돌릴 때 psnr, ssim 를 평균으로 저장하는 함수 (.csv로 저장)
+def save_metrics(metrics, filename):
+    with open(filename, "w") as f:
+        f.write("PSNR,SSIM\n")
+        for i in range(len(metrics["PSNR"])):
+            f.write(f"{metrics['PSNR'][i]},{metrics['SSIM'][i]}\n")
+    print(f"Metrics saved to {filename}")
+
+
+if __name__ == "__main__":
+    # Load the dataset
+    train_dataset, test_dataset, train_loader, test_loader = load_images()
+
+    # Print dataset sizes
+    print(f"Train dataset size: {len(train_dataset)}")
+    print(f"Test dataset size: {len(test_dataset)}")
+
+    # Initialize the model
+    model = PxT()
+    print(model)
+
+    device = torch.device(
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+
+    # use multiple GPUs if available
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
+        print(f"Using {torch.cuda.device_count()} GPUs")
+
+    model.to(device)
+    print(f"Model device: {device}")
+
+    # train the model
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        for i, (input_images, target_images) in enumerate(
+            tqdm.tqdm(train_loader, desc=f"Train Epoch {epoch+1}/{epochs}")
+        ):
+            input_images = input_images.to(device)
+            target_images = target_images.to(device)
+
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(input_images)
+            loss = criterion(outputs, target_images)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        epoch_loss = running_loss / len(train_loader)
+        print(f"Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss:.4f}")
+        logging.info(f"Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss:.4f}")
+        # Save the model
+        if (epoch + 1) % 10 == 0:
+            torch.save(model.state_dict(), f"DnCNN_{epoch+1}.pth")
+            print(f"Model saved at epoch {epoch+1}")
+            logging.info(f"Model saved at epoch {epoch+1}")
+
+    # Test the model
+    model.eval()
+    test_loss = 0.0
+    psnr_values = []
+    ssim_values = []
+    psnr_b_values = []
+
+    with torch.no_grad():
+        for input_images, target_images in tqdm.tqdm(test_loader, desc="Testing"):
+            input_images = input_images.to(device)
+            target_images = target_images.to(device)
+
+            # Forward pass
+            outputs = model(input_images)
+
+            # Calculate MSE loss
+            loss = criterion(outputs, target_images)
+            test_loss += loss.item()
+
+            # bgr tensor to rgb image ndarray
+            target_images = target_images.permute(0, 2, 3, 1).cpu().numpy()
+            outputs = outputs.permute(0, 2, 3, 1).cpu().numpy()
+
+            for i in range(len(outputs)):
+                # [32,32,3]
+                print(f"target image shape: {target_images[i].shape}")
+                print(f"output image shape: {outputs[i].shape}")
+
+                # outputs rgb to bgr and
+                bgr_image = cv2.cvtColor(outputs[i], cv2.COLOR_RGB2BGR)
+                os.makedirs("PxT_output", exist_ok=True)
+
+                # Save the output image
+                cv2.imwrite(os.path.join("PxT_output", f"output_{i}.png"), bgr_image)
+
+                # Calculate PSNR
+                psnr = peak_signal_noise_ratio(
+                    target_images[i], outputs[i], data_range=1.0
+                )
+
+                # Calculate SSIM
+                ssim = structural_similarity(
+                    target_images[i],
+                    outputs[i],
+                    multichannel=True,
+                    data_range=1.0,
+                    channel_axis=2,
+                )
+
+                psnr_values.append(psnr)
+                ssim_values.append(ssim)
+                # psnr_b_values.append(psnr_b)
+                # print(f"PSNR: {psnr:.2f}, SSIM: {ssim:.4f}, PSNR-B: {psnr_b:.2f}")
+                # logging.info(
+                #     f"PSNR: {psnr:.2f}, SSIM: {ssim:.4f}, PSNR-B: {psnr_b:.2f}"
+                # )
+                print(f"PSNR: {psnr:.2f}, SSIM: {ssim:.4f}")
+                logging.info(f"PSNR: {psnr:.2f}, SSIM: {ssim:.4f}")
+
+            # Save the output images
+            for i in range(len(outputs)):
+                output_image = cv2.cvtColor(outputs[i], cv2.COLOR_RGB2BGR)
+                output_image_path = os.path.join("output", f"output_{i}.png")
+                cv2.imwrite(output_image_path, output_image)
+                print(f"Output image saved at {output_image_path}")
+            logging.info(f"Output image saved at {output_image_path}")
+
+    # Calculate average metrics
+    avg_test_loss = test_loss / len(test_loader)
+    avg_psnr = np.mean(psnr_values)
+
+    print(f"Test Loss: {avg_test_loss:.4f}, PSNR: {avg_psnr:.2f} dB")
+    logging.info(f"Test Loss: {avg_test_loss:.4f}, PSNR: {avg_psnr:.2f} dB")
+
+    # Save metrics
+    metrics = {
+        "Test Loss": [avg_test_loss],
+        "PSNR": psnr_values,
+        "SSIM": ssim_values,
+    }
+    save_metrics(metrics, "dncnn_metrics.csv")
+    # Save the final model
+    torch.save(
+        model.state_dict(),
+        os.path.join("datasets", f"{type(model).__name__}_final.pth"),
+    )
+    print(f"Final model saved as {type(model).__name__}_final.pth")
+    logging.info(f"Final model saved as {type(model).__name__}_final.pth")
