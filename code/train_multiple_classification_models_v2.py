5e91cc11fa1138703d50afebd63a2eb9306913ed
엄지호 <say7fish@gmail.com>
2025-04-16 12:45:52 +0900

    test

--- code/train_multiple_classification_models_v2.py
+++ code/train_multiple_classification_models_v2.py
@@ -7,13 +7,21 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 import sys
+import logging
 
-if len(sys.argv) < 3:
-    print("Usage: python train_multiple_classification_models.py <epochs> <batch_size>")
+logging.basicConfig(
+    filename="data.log", level=logging.INFO, format="%(asctime)s - %(message)s"
+)
+
+if len(sys.argv) < 4:
+    print(
+        "Usage: python train_multiple_classification_models.py <epochs> <batch_size> <num_workers>"
+    )
     sys.exit(1)
 
 epochs = int(sys.argv[1])
 batch_size = int(sys.argv[2])
+num_workers = int(sys.argv[3])
 
 # 데이터 준비
 transform = transforms.Compose(
@@ -32,8 +40,37 @@ test_dataset = CIFAR100(
     root="./datasets", train=False, download=True, transform=transform
 )
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+train_loader = DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers
+)
+test_loader = DataLoader(
+    test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers
+)
+
+
+# JPEG 데이터셋 로드
+def load_post_processed_jpeg_datasets(QF, transform, dataset_name):
+    jpeg_test_dir = os.path.join(
+        os.getcwd(), "datasets", "removed_cifar100", dataset_name, f"JPEG{QF}", "test"
+    )
+
+    test_dataset = datasets.ImageFolder(jpeg_test_dir, transform=transform)
+
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers
+    )
+
+    return test_dataset, test_dataloader
+
+
+def sort_key(filename):
+    image_match = re.search(r"image_(\d+)", filename)
+    crop_match = re.search(r"crop_(\d+)", filename)
+
+    image_number = int(image_match.group(1)) if image_match else float("inf")
+    crop_number = int(crop_match.group(1)) if crop_match else float("inf")
+
+    return (image_number, crop_number)
 
 
 # 모델 선택 함수
@@ -92,7 +129,7 @@ def train_model(model_name, epochs=epochs):
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-            pbar.set_postfix({'loss': loss.item()})
+            pbar.set_postfix({"loss": loss.item()})
 
         print(f"Epoch {epoch+1}, Loss: {running_loss / len(train_loader)}")
 
@@ -108,7 +145,37 @@ def train_model(model_name, epochs=epochs):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
+    # model save
+    torch.save(model.state_dict(), f"{model_name}_cifar100.pth")
+    print(f"Model {model_name} saved as {model_name}_cifar100.pth")
     print(f"Accuracy of {model_name} on CIFAR-100: {100 * correct / total:.2f}%")
+    logging.info(f"Accuracy of {model_name} on CIFAR-100: {100 * correct / total:.2f}%")
+
+    # JPEG 데이터셋에 대한 정확도 계산
+    QFs = [100, 80, 60, 40, 20]
+    dataset_names = ["ARCNN", "BlockCNN", "DnCNN", "PxT"]
+    for QF in QFs:
+        for dataset_name in dataset_names:
+            jpeg_test_dataset, jpeg_test_dataloader = load_post_processed_jpeg_datasets(
+                QF, transform, dataset_name
+            )
+
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for inputs, labels in tqdm(jpeg_test_dataloader, desc="Testing"):
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    outputs = model(inputs)
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+
+            print(
+                f"Accuracy of {model_name} on CIFAR-100 JPEG QF {QF}: {100 * correct / total:.2f}%"
+            )
+            logging.info(
+                f"Accuracy of {model_name} on CIFAR-100 JPEG QF {QF}: {100 * correct / total:.2f}%"
+            )
 
 
 # 실행 예시: 원하는 모델 이름을 입력하여 훈련 시작
