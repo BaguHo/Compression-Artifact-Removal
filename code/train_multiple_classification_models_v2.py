158daa17a50b3cc7cb73e0443055fdbaad24a3a8
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2025-04-17 22:49:26 +0900

    .

--- code/train_multiple_classification_models_v2.py
+++ code/train_multiple_classification_models_v2.py
@@ -158,7 +158,7 @@ def get_model(model_name):
             nn.Linear(num_ftrs, 1024),
             nn.ReLU(),
             nn.Linear(1024, 2048),
-            nn.ReLu(),
+            nn.ReLU(),
             nn.Linear(2048, 1024),
             nn.ReLU(),
             nn.Linear(1024, 100),
