2f48efd28905660782884fb4e35cb3a3779eb648
엄지호 <say7fish@gmail.com>
2025-04-20 21:22:01 +0900

    make original mini imagenet

--- code/make_mini_imagenet_dataset.py
+++ code/make_mini_imagenet_dataset.py
@@ -35,6 +35,47 @@ def change_imagenet_dir_name():
             print(f"{folder} -> {new_name}")
 
 
+def make_and_save_mini_imagenet():
+    mini_imagenet_path = os.path.join(os.getcwd(), "datasets", "mini-imagenet")
+    transform = T.Compose([T.ToTensor(), T.Resize((224, 224))])
+
+    # 데이터셋 로드
+    train_dataset = datasets.ImageFolder(
+        os.path.join(mini_imagenet_path, "original", "train"),
+        transform=transform,
+    )
+    test_dataset = datasets.ImageFolder(
+        os.path.join(mini_imagenet_path, "original", "test"),
+        transform=transform,
+    )
+
+    output_train_path = os.path.join(mini_imagenet_path, f"_original", "train")
+    output_test_path = os.path.join(mini_imagenet_path, f"_original", "test")
+    for i in range(1000):
+        os.makedirs(os.path.join(output_train_path, str(i)), exist_ok=True)
+        os.makedirs(os.path.join(output_test_path, str(i)), exist_ok=True)
+
+    for idx, (img, label) in enumerate(train_dataset):
+        image = T.ToPILImage()(img)
+        image_filename = os.path.join(
+            output_train_path, str(label), f"image_{int(idx):05d}.png"
+        )
+        image.save(image_filename, "PNG")
+
+        if idx % 5000 == 0:
+            print(f"{idx} training images saved...")
+
+    for idx, (img, label) in enumerate(test_dataset):
+        image = T.ToPILImage()(img)
+        image_filename = os.path.join(
+            output_test_path, str(label), f"image_{int(idx):05d}.png"
+        )
+        image.save(image_filename, "PNG")
+
+        if idx % 5000 == 0:
+            print(f"{idx} testing images saved...")
+
+
 def make_and_save_mini_imagenet_each_qf(QF):
     mini_imagenet_path = os.path.join(os.getcwd(), "datasets", "mini-imagenet")
     transform = T.Compose([T.ToTensor(), T.Resize((224, 224))])
@@ -49,8 +90,8 @@ def make_and_save_mini_imagenet_each_qf(QF):
         transform=transform,
     )
 
-    output_train_path = os.path.join(mini_imagenet_path, "_original", "train")
-    output_test_path = os.path.join(mini_imagenet_path, "_original", "test")
+    output_train_path = os.path.join(mini_imagenet_path, f"jpeg{QF}", "train")
+    output_test_path = os.path.join(mini_imagenet_path, f"jpeg{QF}", "test")
     for i in range(1000):
         os.makedirs(os.path.join(output_train_path, str(i)), exist_ok=True)
         os.makedirs(os.path.join(output_test_path, str(i)), exist_ok=True)
@@ -79,4 +120,6 @@ def make_and_save_mini_imagenet_each_qf(QF):
 if __name__ == "__main__":
     QFs = [100, 80, 60, 40, 20]
     change_imagenet_dir_name()
-    make_and_save_mini_imagenet_each_qf(100)
+    make_and_save_mini_imagenet()
+    for qf in QFs:
+        make_and_save_mini_imagenet_each_qf(qf)
