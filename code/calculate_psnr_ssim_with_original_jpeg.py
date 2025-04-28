a6542b89a13b8b90f8d55056256d474d6e090416
say7fish <say7fish@gmail.com>
2025-04-28 14:36:09 +0900

    .

--- code/calculate_psnr_ssim_with_original_jpeg.py
+++ code/calculate_psnr_ssim_with_original_jpeg.py
@@ -7,9 +7,6 @@ import lpips
 
 def calculate_avg_psnr_ssim(original_image_dir, removed_image_dir):
     lpips_model = lpips.LPIPS(net='alex')
-    psnr_scores_original = []  # original vs original
-    ssim_scores_original = []  # original vs original
-    lpips_scores_original = []  # original vs original
     psnr_scores_removed = []  # original vs removed
     ssim_scores_removed = []  # original vs removed
     lpips_scores_removed = []  # original vs removed
@@ -26,15 +23,6 @@ def calculate_avg_psnr_ssim(original_image_dir, removed_image_dir):
         original_image = np.array(Image.open(original_path))
         removed_image = np.array(Image.open(removed_path))
 
-        # Calculate metrics between original and itself
-        psnr_original = psnr(original_image, original_image)
-        ssim_original = ssim(
-            original_image, 
-            original_image,
-            win_size=3,  # 작은 이미지를 위해 win_size를 줄임
-            channel_axis=2  # RGB 이미지의 경우 일반적으로 채널이 마지막 축(2)에 있음
-        )
-
         # Calculate metrics between original and removed
         psnr_removed = psnr(original_image, removed_image)
         ssim_removed = ssim(
@@ -45,25 +33,18 @@ def calculate_avg_psnr_ssim(original_image_dir, removed_image_dir):
         )
 
         # Calculate LPIPS between original and removed
-        lpips_original = lpips_model(original_image, original_image)
         lpips_removed = lpips_model(original_image, removed_image)
 
-        psnr_scores_original.append(psnr_original)
-        ssim_scores_original.append(ssim_original)
-        lpips_scores_original.append(lpips_original)
         psnr_scores_removed.append(psnr_removed)
         ssim_scores_removed.append(ssim_removed)
         lpips_scores_removed.append(lpips_removed)
 
     # Calculate averages
-    avg_psnr_original = np.mean(psnr_scores_original)
-    avg_ssim_original = np.mean(ssim_scores_original)
     avg_psnr_removed = np.mean(psnr_scores_removed)
     avg_ssim_removed = np.mean(ssim_scores_removed)
-    avg_lpips_original = np.mean(lpips_scores_original)
     avg_lpips_removed = np.mean(lpips_scores_removed)
 
-    return avg_psnr_original, avg_ssim_original, avg_psnr_removed, avg_ssim_removed, avg_lpips_original, avg_lpips_removed
+    return avg_psnr_removed, avg_ssim_removed, avg_lpips_removed
 
     
 if __name__ == "__main__":
@@ -84,7 +65,7 @@ if __name__ == "__main__":
                 os.getcwd(), "datasets", "mini-imagenet", f"jpeg{QF}", "test", str(i)
             )
 
-            avg_psnr_original, avg_ssim_original, avg_psnr_removed, avg_ssim_removed, avg_lpips_original, avg_lpips_removed = (
+            avg_psnr_removed, avg_ssim_removed, avg_lpips_removed = (
                 calculate_avg_psnr_ssim(original_image_dir, removed_image_dir)
             )
 
@@ -98,11 +79,11 @@ if __name__ == "__main__":
                     == 0
                 ):
                     f.write(
-                        "QF,class,avg_psnr_original,avg_ssim_original,avg_psnr_removed,avg_ssim_removed,avg_lpips_original,avg_lpips_removed\n"
+                        "QF,class,avg_psnr_removed,avg_ssim_removed,avg_lpips_removed\n"
                     )
                 f.write(
-                    f"{QF},{i},{avg_psnr_original},{avg_ssim_original},{avg_psnr_removed},{avg_ssim_removed},{avg_lpips_original},{avg_lpips_removed}\n"
+                    f"{QF},{i},{avg_psnr_removed},{avg_ssim_removed},{avg_lpips_removed}\n"
                 )
             print(
-                f"QF: {QF}, class: {i}, avg_psnr_original: {avg_psnr_original}, avg_ssim_original: {avg_ssim_original}, avg_psnr_removed: {avg_psnr_removed}, avg_ssim_removed: {avg_ssim_removed}, avg_lpips_original: {avg_lpips_original}, avg_lpips_removed: {avg_lpips_removed}"
+                f"QF: {QF}, class: {i}, avg_psnr_removed: {avg_psnr_removed}, avg_ssim_removed: {avg_ssim_removed}, avg_lpips_removed: {avg_lpips_removed}"
             )
