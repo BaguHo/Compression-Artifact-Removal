fafe0c93e8c20425c95f2857808128707523ed4e
say7fish <say7fish@gmail.com>
2025-04-28 14:44:14 +0900

    .

--- code/calculate_psnr_ssim_with_original_jpeg.py
+++ code/calculate_psnr_ssim_with_original_jpeg.py
@@ -1,90 +1,49 @@
 import numpy as np
-import os
+import os, sys, logging
 from PIL import Image
 from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.metrics import structural_similarity as ssim
 import lpips
-import torch
 
-def calculate_avg_psnr_ssim(original_image_dir, removed_image_dir):
-    lpips_model = lpips.LPIPS(net='alex')
-    psnr_scores_removed = []  # original vs removed
-    ssim_scores_removed = []  # original vs removed
-    lpips_scores_removed = []  # original vs removed
 
-    # Get list of files in each directory
-    original_files = sorted(os.listdir(original_image_dir))
-    removed_files = sorted(os.listdir(removed_image_dir))
+def calculate_psnr_ssim_lpips(QF):
+    original_dataset_path  = os.path.join("datasets/mini-imagenet/_original/test")
+    jpeg_dataset_path = os.path.join("datasets/mini-imagenet/jpeg{QF}/test")
 
-    for original_file, removed_file in zip(original_files, removed_files):
-        original_path = os.path.join(original_image_dir, original_file)
-        removed_path = os.path.join(removed_image_dir, removed_file)
+    original_images = []
+    jpeg_images = []
 
-        # Load images and convert to numpy arrays
-        original_image = np.array(Image.open(original_path))
-        removed_image = np.array(Image.open(removed_path))
+    for i in range(1000):
+        original_image_path = os.path.join(original_dataset_path, str(i))
+        jpeg_image_path = os.path.join(jpeg_dataset_path, str(i))
 
-        # Calculate metrics between original and removed
-        psnr_removed = psnr(original_image, removed_image)
-        ssim_removed = ssim(
-            original_image, 
-            removed_image,
-            win_size=3,  # 작은 이미지를 위해 win_size를 줄임
-            channel_axis=2  # RGB 이미지의 경우 채널이 마지막 축에 있음
-        )
+        original_image = Image.open(original_image_path)
+        jpeg_image = Image.open(jpeg_image_path)
 
-        # Calculate LPIPS between original and removed
-        lpips_removed = lpips_model(torch.from_numpy(original_image), torch.from_numpy(removed_image))
+        original_images.append(original_image)
+        jpeg_images.append(jpeg_image)
 
-        psnr_scores_removed.append(psnr_removed)
-        ssim_scores_removed.append(ssim_removed)
-        lpips_scores_removed.append(lpips_removed)
+    lpips_metric = lpips.LPIPS(net="alex")
+    lpips_values = []
+    psnr_values = []
+    ssim_values = []
 
-    # Calculate averages
-    avg_psnr_removed = np.mean(psnr_scores_removed)
-    avg_ssim_removed = np.mean(ssim_scores_removed)
-    avg_lpips_removed = np.mean(lpips_scores_removed)
+    for i in range(1000):
+        original_image = original_images[i]
+        jpeg_image = jpeg_images[i]
 
-    return avg_psnr_removed, avg_ssim_removed, avg_lpips_removed
+        original_image = np.array(original_image)
+        jpeg_image = np.array(jpeg_image)
 
-    
+        lpips_values.append(lpips_metric(original_image, jpeg_image))
+        psnr_values.append(psnr(original_image, jpeg_image))
+        ssim_values.append(ssim(original_image, jpeg_image))
+
+    print(f"QF {QF}: PSNR = {np.mean(psnr_values)}, SSIM = {np.mean(ssim_values)}, LPIPS = {np.mean(lpips_values)}")
+    logging.info(f"QF {QF}: PSNR = {np.mean(psnr_values)}, SSIM = {np.mean(ssim_values)}, LPIPS = {np.mean(lpips_values)}")
+    return np.mean(psnr_values), np.mean(ssim_values), np.mean(lpips_values)
+
 if __name__ == "__main__":
-    QFs = [100, 80, 60, 40, 20]
-    num_classes = 1000
-
+    QFs = ["100", "80", "60", "40", "20"]
     for QF in QFs:
-        for i in range(num_classes):
-            original_image_dir = os.path.join(
-                os.getcwd(),
-                "datasets",
-                "mini-imagenet",
-                f"_original",
-                "test",
-                str(i),
-            )
-            removed_image_dir = os.path.join(
-                os.getcwd(), "datasets", "mini-imagenet", f"jpeg{QF}", "test", str(i)
-            )
-
-            avg_psnr_removed, avg_ssim_removed, avg_lpips_removed = (
-                calculate_avg_psnr_ssim(original_image_dir, removed_image_dir)
-            )
-
-            with open(
-                os.path.join(os.getcwd(), "results", "psnr_ssim_results.csv"), "a"
-            ) as f:
-                if (
-                    os.path.getsize(
-                        os.path.join(os.getcwd(), "results", "psnr_ssim_results.csv")
-                    )
-                    == 0
-                ):
-                    f.write(
-                        "QF,class,avg_psnr_removed,avg_ssim_removed,avg_lpips_removed\n"
-                    )
-                f.write(
-                    f"{QF},{i},{avg_psnr_removed},{avg_ssim_removed},{avg_lpips_removed}\n"
-                )
-            print(
-                f"QF: {QF}, class: {i}, avg_psnr_removed: {avg_psnr_removed}, avg_ssim_removed: {avg_ssim_removed}, avg_lpips_removed: {avg_lpips_removed}"
-            )
+        calculate_psnr_ssim_lpips(QF)
